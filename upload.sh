0638ac85a6d67e29cf3d69ea79c822f189208ac9
erik aronesty <erik@q32.com>
2023-09-14 13:50:41 -0400

    upl

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp dist/gputopia-worker-* s3://gputopia/bin/gputopia-worker-cuda-linux-64 --acl public-read
+aws s3 cp dist/gputopia-worker-* s3://gputopia/bin/ --acl public-read
