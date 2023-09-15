f934d11dbc37b98ee091c47f5075d82cb61ecc1b
erik aronesty <erik@q32.com>
2023-09-15 15:36:15 -0400

    fix worker unload before load

--- ai_worker/main.py
+++ ai_worker/main.py
@@ -14,8 +14,9 @@ import psutil
 import websockets
 from httpx import Response, AsyncClient
 from httpx_sse import aconnect_sse
-from llama_cpp.server.app import Settings as LlamaSettings, create_app as create_llama_app, llama as global_llama
-from llama_cpp import llama_free_model
+from llama_cpp.server.app import Settings as LlamaSettings, create_app as create_llama_app
+import llama_cpp.server.app
+from llama_cpp import llama_free_model, llama_free
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pynvml.smi import nvidia_smi
@@ -167,11 +168,9 @@ class WorkerMain:
         log.debug("loading model: %s", name)
         model_path = await self.get_model(name)
 
-        if global_llama:
-            llama_free_model(global_llama.model)
-            llama_free_model(global_llama.ctx)
-            global_llama.model = None
-            global_llama.ctx = None
+        if llama_cpp.server.app.llama:
+            # critical... must del this before creating a new app
+            del llama_cpp.server.app.llama
 
         settings = LlamaSettings(model=model_path, n_gpu_layers=await self.guess_layers(model_path), seed=-1,
                                  embedding=True, cache=True, low_vram=self.conf.low_vram, port=8181)
