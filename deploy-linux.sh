5403212e0b4e142188ab11e044bb53f740f670c3
erik <erik@q32.com>
2023-09-15 13:23:23 +0000

    more build stuff

---
+++ deploy-linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+set -o xtrace
+
+python -mvenv build-cuda
+. build-cuda/bin/activate
+
+pip uninstall -y llama-cpp-python
+rm -f ~/.cache/pypoetry/artifacts/*/*/*/*/llama*
+
+CMAKE_ARGS="-DLLAMA_CUBLAS=1" FORCE_CMAKE=1 poetry install
+./pyinstaller.sh cuda-linux-64
+
+deactivate
+
+
+python -mvenv build-opencl
+. build-opencl/bin/activate
+
+pip uninstall -y llama-cpp-python
+rm -f ~/.cache/pypoetry/artifacts/*/*/*/*/llama*
+
+CMAKE_ARGS="-DLLAMA_CLBLAST=ON" FORCE_CMAKE=1 poetry install
+./pyinstaller.sh opencl-linux-64
+
+deactivate
+
+#./upload.sh
--- deploy-windows.sh
+++ deploy-windows.sh
@@ -5,16 +5,24 @@ set -o xtrace
 python -mvenv build-cuda
 
 . build-cuda/scripts/activate
-CMAKE_ARGS="-DLLAMA_CUBLAS=1" FORCE_CMAKE=1 poetry install
+pip uninstall -y llama-cpp-python
+rm -f ~/AppData/Local/pypoetry/Cache/pypoetry/artifacts/*/*/*/*/llama*
 
+CMAKE_ARGS="-DLLAMA_CUBLAS=1" FORCE_CMAKE=1 poetry install
 ./pyinstaller.sh cuda-win-64
+
 deactivate
 
 
 python -mvenv build-opencl
 . build-opencl/scripts/activate
+
+pip uninstall -y llama-cpp-python
+rm -f ~/AppData/Local/pypoetry/Cache/pypoetry/artifacts/*/*/*/*/llama*
+
 CMAKE_ARGS="-DLLAMA_CLBLAST=ON -DCMAKE_PREFIX_PATH=C:/CLBlast/lib/cmake/CLBlast" FORCE_CMAKE=1 poetry install
 ./pyinstaller.sh opencl-win-64
+
 deactivate
 
 ./upload.sh
