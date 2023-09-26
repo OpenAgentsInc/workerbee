f3f90bab50030ee8ec52b2af5953feced6056f04
erik aronesty <erik@q32.com>
2023-09-26 12:34:29 -0400

    release script

--- publish-github.sh
+++ publish-github.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
+#!/bin/bash -e
 version=$1
 if [ -z "$version" ]; then
-    echo usage: $0 <version>
+    echo "usage: $0 <version>"
+    exit 1
 fi
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "need a GITHUB_TOKEN env var or .env"
+fi
+
+if ! grep $version pyproject.toml; then
+    echo "version $version not in pyproject.toml"
+    exit 1
+fi
+
+git fetch --tags
+
+if git tag | grep $version; then
+    echo "already have a $version"
+fi
+
 rm -rf bin
-aws s3 cp s3://gputopia/bin/* bin/
-githubrelease release arcadelabsinc/workerbee create 2.0.0 --publish --name "gputopia-worker-$version" "bin/*"
+aws s3 sync "s3://gputopia/bin/" bin/
+githubrelease release arcadelabsinc/workerbee create "$version" --publish --name "gputopia-worker-$version" "bin/*"
--- upload.sh
+++ upload.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if ! aws sts get-caller-identity --query "Account"; then
+	aws sso login
+fi
+
 
 for f in dist/gputopia-worker-*; do
     aws s3 cp $f s3://gputopia/bin/ --acl public-read
