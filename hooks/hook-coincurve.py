669cd22a66c47520f657188d187d7f659fed4709
erik <erik@q32.com>
2023-10-26 18:31:37 +0000

    hook

--- hooks/hook-coincurve.py
+++ hooks/hook-coincurve.py
@@ -1,6 +1,8 @@
 from PyInstaller.utils.hooks import collect_data_files, get_package_paths
 import os
 
+hiddenimports=['_cffi_backend']
+
 # Get the package path
 package_path = get_package_paths('coincurve')[0]
 
@@ -12,6 +14,14 @@ if os.name == 'nt':  # Windows
     dll_path = os.path.join(package_path, 'coincurve', 'libsecp256k1.dll')
     datas.append((dll_path, 'coincurve'))
 elif os.name == 'posix':  # Linux/Mac
-    so_path = os.path.join(package_path, 'coincurve', 'libsecp256k1.so')
+    so_paths =[
+            os.path.join(package_path, 'coincurve', 'libsecp256k1.so'),
+            os.path.join(package_path, 'coincurve', '_libsecp256k1.cpython-311-x86_64-linux-gnu.so')
+    ]
+    so_path = None
+    for p in so_paths:
+        if os.path.exists(p):
+            so_path = p
+    assert so_path, f"can't find coincurve lib, look in {package_path} and then modify this hook" 
     datas.append((so_path, 'coincurve'))
 
