96a1f4652e766db3f10e2ba47e12fe82e92bce44
erik <erik@q32.com>
2023-11-13 18:44:32 +0000

    ctv

--- scripts/convert_civit.py
+++ scripts/convert_civit.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 from diffusers import StableDiffusionXLPipeline
@@ -8,8 +9,9 @@ if __name__ == "__main__":
     name = sys.argv[2]
     url = "gputopia/" + name
     hashname = "sdxl." + hashlib.md5(url.encode()).hexdigest()
-    subprocess.run("wget -O tmp.st https://civitai.com/api/download/models/" + modid, shell=True)
-    pipe = StableDiffusionXLPipeline.from_single_file("tmp.st")
+    if not os.path.exists(f"{hashname}.st"):
+        subprocess.run(f"wget -O {hashname}.st https://civitai.com/api/download/models/" + modid, shell=True)
+    pipe = StableDiffusionXLPipeline.from_single_file(f"{hashname}.st", use_safetensors=True)
     pipe.save_pretrained(hashname)
-    subprocess.run("tar -cf - {hashname}/ | pigz -9 - > {hashname}.tar.gz", shell=True)
-    subprocess.run("aws s3 cp {hashname}.tar.gz s3://gputopia/models/{name}.tar.gz", shell=True)
+    subprocess.run(f"tar -cf - {hashname}/ | pigz -9 - > {hashname}.tar.gz", shell=True)
+    subprocess.run(f"aws s3 cp {hashname}.tar.gz s3://gputopia/models/{name}.tar.gz", shell=True)
