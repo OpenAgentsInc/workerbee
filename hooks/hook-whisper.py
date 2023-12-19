3114e8b068ed207cfcc02cd3c415ffad8b678819
Gonzalo Martinez <1536784+gonzafirewall@users.noreply.github.com>
2023-12-18 23:50:25 -0300

    fix path

--- hooks/hook-whisper.py
+++ hooks/hook-whisper.py
@@ -10,14 +10,14 @@ datas = collect_data_files('nvidia')
 # Append the additional .dll or .so file
 if os.name == 'nt':  # Windows
     # need to check because i dont have a gpu with windows to test
-    dll_path = os.path.join(package_path, 'cudnn', 'lib', 'libcudnn_ops_infer.dll')
+    dll_path = os.path.join(package_path, 'nvidia', 'cudnn', 'lib', 'libcudnn_ops_infer.dll')
     datas.append((dll_path, '.'))
-    dll_path = os.path.join(package_path, 'cudnn', 'lib', 'libcudnn_cnn_infer.dll')
+    dll_path = os.path.join(package_path, 'nvidia', 'cudnn', 'lib', 'libcudnn_cnn_infer.dll')
     datas.append((dll_path, '.'))
 elif os.name == 'posix':  # Linux/Mac
-    so_path = os.path.join(package_path, 'cudnn', 'lib', 'libcudnn_ops_infer.so.8')
+    so_path = os.path.join(package_path, 'nvidia', 'cudnn', 'lib', 'libcudnn_ops_infer.so.8')
     datas.append((so_path, '.'))
-    so_path = os.path.join(package_path, 'cudnn', 'lib', 'libcudnn_cnn_infer.so.8')
+    so_path = os.path.join(package_path, 'nvidia', 'cudnn', 'lib', 'libcudnn_cnn_infer.so.8')
     datas.append((so_path, '.'))
 
 
